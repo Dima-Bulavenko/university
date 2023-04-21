5533978066f29ee145a135918e77d8c97b8bc897
Dima Bulavenko <dima99bylovenko@gmail.com>
2023-04-21 12:07:05 +0100

    Create view for: deliting student by student id adding student to a course by course and student id removing student from a course by course and student id

--- university/university.py
+++ university/university.py
@@ -27,6 +27,23 @@ class StudentsApi(Resource):
             return make_response({'message': f"{ex}"})
 
 
+class StudentApi(Resource):
+    def delete(self, student_id):
+        try:
+
+            student = session.query(Student).get(student_id)
+            if not student:
+                raise Exception(f"Student id '{student_id}' was not found")
+            session.delete(student)
+            session.commit()
+            return make_response(
+                {"message": f'Student with {student_id} was deleted'})
+
+        except Exception as ex:
+            session.rollback()
+            return make_response({'error': f"{ex}"})
+
+
 class GroupsApi(Resource):
     def get(self, stud_count):
         groups = (session.query(Group)
@@ -51,12 +68,63 @@ class CourseStudentsApi(Resource):
 
         return make_response(Student.create_dict_for_api(students))
 
+    def put(self, course_id, student_id):
+        try:
+            course = session.get(Course, course_id)
+            student = session.get(Student, student_id)
+            if not course:
+                raise Exception(f"Course id '{course_id}' does not exist")
 
-api.add_resource(GroupsApi, '/groups/<int:stud_count>', endpoint='groups_api')
+            if not student:
+                raise Exception(f"Student id '{student_id}' does not exist")
+
+            session.add(StudentCourse(student_id=student.id,
+                                      course_id=course.id))
+            session.commit()
+            return make_response(
+                {"message": f"Student {student.first_name} was added to course {course.name}"})
+        except Exception as ex:
+            session.rollback()
+            return make_response({"error": f"{ex}"}, 400)
+
+    def delete(self, course_id, student_id):
+        try:
+            course = session.get(Course, course_id)
+            student = session.get(Student, student_id)
+            if not course:
+                raise Exception(f"Course id '{course_id}' does not exist")
+
+            if not student:
+                raise Exception(f"Student id '{student_id}' does not exist")
+            student_course = (session
+                              .query(StudentCourse)
+                              .filter(StudentCourse.student_id == student.id,
+                                      StudentCourse.course_id == course.id)
+                              .one())
+            session.delete(student_course)
+            session.commit()
+            return make_response(
+                {"message": f"Student {student.first_name} was deleted from course {course.name}"})
+        except Exception as ex:
+            session.rollback()
+            return make_response({"error": f"{ex}"}, 400)
+
+
+api.add_resource(GroupsApi,
+                 '/groups/<int:stud_count>',
+                 endpoint='groups_api')
 api.add_resource(CourseStudentsApi,
                  '/courses/<int:course_id>/students',
-                 endpoint='course_students_api')
-api.add_resource(StudentsApi, "/students", endpoint='students_api')
+                 endpoint='get_students_from_course')
+api.add_resource(CourseStudentsApi,
+                 '/courses/<int:course_id>/<int:student_id>',
+                 endpoint='add_delete_course_for_student')
+api.add_resource(StudentsApi,
+                 "/students",
+                 endpoint='students_api')
+api.add_resource(StudentApi,
+                 "/students/<int:student_id>",
+                 endpoint='student_api')
 
 
 if __name__ == "__main__":
